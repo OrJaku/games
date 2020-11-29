d30ac64accac4f60f2c096c52debba8d6ab6859d
OrJaku <kuba.orzel@hotmail.com>
2020-11-29 17:50:10 +0100

    Sudoku- ip progress

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,10 +1,16 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def board_game():
+    if request.method == "POST":
+        elements = request.form
+        print("Elements", elements)
+        for i in elements:
+            
+        
     return render_template("board.html")
 
 if __name__ == '__main__':
